f95bedfe4f27b018a9987ddd534da22830e292ea
Kauã Steliano <stelianok@gmail.com>
2020-12-28 15:05:28 -0300

    Mail template finished

---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,9 @@
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+export default class FakeMailTemplateProvider implements IMailTemplateProvider {
+    public async parse(): Promise<string> {
+        return 'Mail template';
+    }
+
+}
