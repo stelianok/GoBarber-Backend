24745d4655f529b9f2e5fe29084aaf4b054573ba
Kauã Steliano <stelianok@gmail.com>
2021-01-04 10:28:42 -0300

    Implemented S3 for Content delivery.

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,5 +1,8 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { Exclude, Expose } from 'class-transformer';
+
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class User {
     @PrimaryGeneratedColumn('uuid')
@@ -26,9 +29,17 @@ class User {
 
     @Expose({ name: 'avatar_url' })
     getAvatarUrl(): string | null {
-        return this.avatar
-            ? `${process.env.APP_API_URL}/${this.avatar}`
-            : null
+        if (!this.avatar) {
+            return null;
+        }
+        switch (uploadConfig.driver) {
+            case 'disk':
+                return `${process.env.APP_API_URL}/${this.avatar}`
+            case 's3':
+                return `http://${uploadConfig.config.aws.bucket}.s3.amazonaws/${this.avatar}`
+            default:
+                return null;
+        }
     }
 }
 
