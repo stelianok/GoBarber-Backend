c375faeb8e4298e755c82d5681bf35f429082fed
Kauã Steliano <stelianok@gmail.com>
2021-01-08 09:13:51 -0300

    configured cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,8 +16,11 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.APP_WEB_URL,
+}));
+
 app.use(rateLimiter);
-app.use(cors());
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
